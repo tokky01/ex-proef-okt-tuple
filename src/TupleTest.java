88bd0213799f6adb6d2cc87b4a1bf28e5ebd2979
tokky01 <war.verlinde@gmail.com>
2022-10-18 18:03:43 +0200

    made some fixes

--- src/TupleTest.java
+++ src/TupleTest.java
@@ -5,12 +5,16 @@ import org.junit.Test;
 public class TupleTest {
 	@Test
 	public void test() {
-		Tuple tuple1 = new Tuple(1,4,false);
-		Tuple tuple2 = new Tuple(1,4,false);
+		
+		Object bla2 = "bla";
+		Tuple tuple1 = new Tuple(bla2,4,false);
+		Tuple tuple2 = new Tuple("bla",4,false);
 		assertEquals(tuple1,tuple2);
 		
 		Object bla = tuple1.getValueAt(0);
 		bla = 2;
+		bla2= "bli";
+		
 		assertEquals(bla,2);
 		assertEquals(tuple1,tuple2);
 		
